a1c0ffa1fc343f322c9bbe9801b1ba7c9e0ca7eb
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-03-10 03:33:08 -0500

    Updated headers

--- EditDistance.h
+++ EditDistance.h
@@ -1,14 +1,13 @@
 #ifndef EDIT_DISTANCE_H
 #define EDIT_DISTANCE_H
 
-
 /*Costs Per Possibility*/
-const int gap = 2;
-const int mismatch = 1;
-const int match = 0;
+#define  GAP  2
+#define MISMATCH 1
+#define MATCH 0
 
-/* Return i,j'th index of matrix*/
-int index(int row, int col, int width);
+/* Return i,j'th element of the matrix*/
+int ind(int row, int col, int width);
 
 /* Cost of aligning a and b*/
 int penalty(char a, char b);
@@ -16,12 +15,12 @@ int penalty(char a, char b);
 int min(int a, int b, int c);
 
 /*Populates the matrix based on the two strings*/
-int populate_matrix(char* a, char* b, int* matrix);
+void populate_matrix(char* a, char* b, int* matrix);
 
 /*Back-traces the matrix and returns the alignment*/
 int alignment(char* a, char* b);
 
-void print_matrix(int* matrix);
+void print_matrix(int n,int m,int* matrix);
   
 #endif
 
