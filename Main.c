022c4a445bf26158e07880f920fe9e2d1154bff3
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-03-11 23:56:18 -0400

    Updated Driver

--- Main.c
+++ Main.c
@@ -7,21 +7,21 @@
 #define BUF 1024
 
 int main(int argc, char* argv[]){
-
   //char c[BUF], d[BUF];
-
+  
   //fgets(c, sizeof(c), stdin);
   //fgets(d, sizeof(d), stdin);
-
-  char* a = "AACAGTTACC";
-  char* b = "TAAGGTCA";
   
-  int* matrix = malloc(sizeof(int) * (strlen(a)+1) * (strlen(b)+1));
+  char* a = "AACAGTTACC-";
+  char* b = "TAAGGTCA-";
+
+  int* matrix = malloc(sizeof(int) * (strlen(a)) * (strlen(b)));
   
   populate_matrix(a, b, matrix);
   
-  print_matrix(strlen(a)+1,strlen(b)+1,matrix);
+  print_matrix(strlen(a),strlen(b),matrix);
   
   free(matrix);
+  
   return 0;
 }
