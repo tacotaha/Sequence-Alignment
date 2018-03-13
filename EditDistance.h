854b766679ec079b498b727fd9c3aa62f30c31fa
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-03-12 23:42:09 -0400

    Updated Header

--- EditDistance.h
+++ EditDistance.h
@@ -13,15 +13,22 @@ int ind(int row, int col, int width);
 /* Cost of aligning a and b*/
 int penalty(char a, char b);
 
-int min(int a, int b, int c);
-
 /*Populates the matrix based on the two strings*/
 void populate_matrix(char* a, char* b, int* matrix);
 
 /*Back-traces the matrix and returns the alignment*/
-void alignment(char*,char*,char*,char*,int* matrix);
+void alignment(char* a,char* b,char* a_aligned,char* b_aligned,int* matrix);
+
+/* Print strings aligned vertically*/
+void print_alignment(char* a, char* b);
+
+/* Precondition: populate_matrix() must be called prior to this 
+   Returns: the edit distance between the two strings represented
+   by the matrix*/
+int edit_distance(int* matrix);
+
+int min(int a, int b, int c);
 
 void print_matrix(int n,int m,int* matrix);
-  
 #endif
 
