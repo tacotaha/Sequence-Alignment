ed5cb5ef889132473588aa358fa1ab10e4f34fb2
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-03-13 15:20:08 -0400

    Added verbose option

--- Main.c
+++ Main.c
@@ -2,13 +2,18 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
-
 #include "EditDistance.h"
+
+/*Change this value to be roughly proprtional 
+  to the square root of the amount of available
+  Mem. (in bytes)...*/
 #define BUFFER 100000
 
-int main(int argc, char* argv[]){
+int main(int argc, char** argv){
   char a[BUFFER], b[BUFFER], a_[BUFFER+1], b_[BUFFER+1],c;
   int z = 0;
+
+  const int verbose = argc > 1 && strcmp(argv[1],"-v") == 0 ? 1 : 0;
   
   clock_t start, end;
   double elapsed_time;
@@ -38,19 +43,23 @@ int main(int argc, char* argv[]){
   start = clock();
   
   populate_matrix(a,b,matrix);
-  
-  printf("Calculating Alignment...\n");
-  //print_matrix(a_len,b_len,matrix);
+
+  if(verbose){
+    printf("Calculating Alignment...\n");
+    //print_matrix(a_len,b_len,matrix);
+  }
   
   alignment(a,b,a_,b_,matrix);
   
   print_alignment(a_,b_);
-  
-  end = clock();
-  elapsed_time = ((double) (end - start)) / CLOCKS_PER_SEC;
-  
-  printf("Elapsed Time: %fms\n",elapsed_time);
-  printf("Edit Distance between strings: %d\n",edit_distance(matrix));
+
+  if(verbose){
+    end = clock();
+    elapsed_time = ((double) (end - start)) / CLOCKS_PER_SEC;
+    
+    printf("Elapsed Time: %fms\n",elapsed_time);
+    printf("Edit Distance between strings: %d\n",edit_distance(matrix));
+  }
   
   free(matrix);
   
